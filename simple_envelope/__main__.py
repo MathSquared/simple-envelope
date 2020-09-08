55dd7be8307732a50173cd6c54ac0b9b5d4333ce
Alex Meed <MathSquared@users.noreply.github.com>
2020-09-07 23:52:48 -0500

    Finish main and add rudimentary output

--- simple_envelope/__main__.py
+++ simple_envelope/__main__.py
@@ -1,9 +1,13 @@
 from .constants import DEFAULT_SPECS
+from .generate import make_envelope
 from .input import get_input
 
 def main():
     specs = dict(DEFAULT_SPECS)
     specs.update(get_input())
+    pdf_bytes = make_envelope(specs)
+    with open('output.pdf', 'wb') as fout:
+        fout.write(pdf_bytes)
 
 
 if __name__ == '__main__':
